0c1efa9d164865345f6569a9fe137c20b221199f
adechlien <jhalejandro@uninorte.edu.co>
2024-10-08 13:21:52 -0500

    first time is real finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
 	theme: {
 		extend: {},
 		fontFamily: {
@@ -11,7 +11,13 @@ export default {
 		boxShadow: {
 			'barShadow': '#121212 2px 2px 5px 0px',
 			'barHardShadow': '#121212 4px 4px 5px 0px',
-		}
+		},
+    colors: {
+      'white': '#FFFFFF',
+      'dark-bg': '#121B32',
+      'dark-text': '#98CCF3',
+      'card': '#343467',
+    }
 	},
 	plugins: [],
 }
