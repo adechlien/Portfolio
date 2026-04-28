b296f41bd184b11b873c51e73f1b776defd16ef7
adechlien <jhalejandro@uninorte.edu.com>
2026-04-28 08:30:11 -0500

    updated cv and compressed my photo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,11 +8,6 @@ export default {
     extend: {},
     fontFamily: {
       bellota: ["Bellota Text", "sans-serif"],
-      bebas: ["Bebas Neue", "ui-sans-serif", "system-ui"],
-    },
-    boxShadow: {
-      barShadow: "#121212 2px 2px 5px 0px",
-      barHardShadow: "#121212 4px 4px 5px 0px",
     },
   },
   plugins: [],
